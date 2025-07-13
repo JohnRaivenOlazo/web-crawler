845230b3870eb4069d35dd6e6273333b5ad7e77b
JohnRaivenOlazo <johnraivenolazo@gmail.com>
2025-07-13 22:31:57 +0800

    Add getURLsFromHTML function and corresponding tests; update package.json for jsdom dependency

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require("jsdom")
+
+const getURLsFromHTML = ( htmlBody, baseURL )  => {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll("a")
+    for (const link of linkElements){
+        if(link.href.slice(0, 1) === "/"){
+            //relative
+            try{ 
+                const url = new URL(`${baseURL}${link.href}`);
+                urls.push(url.href)
+            } catch (error) {
+                console.log("Invalid URL relative: ", error.mesage)
+            }
+        }else {
+            //absolute
+            try{ 
+                const url = new URL(link);
+                urls.push(url.href)
+            } catch (error) {
+                console.log("Invalid URL absolute: ", error.message)
+            }
+        }
+    } 
+    return urls
+}
+
 const normalizeURL = (urlString) => {
     const url = new URL(urlString)
     const fullURL = `${url.hostname}${url.pathname}`;
@@ -5,5 +33,6 @@ const normalizeURL = (urlString) => {
 }
 
 module.exports  =  {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalize URL strip protocol", () => {
@@ -19,5 +19,73 @@ test("normalize URL strip http", () => {
     const input =  "http://Raiven.com/path/"
     const actual = normalizeURL(input)
     const expected = "raiven.com/path"
+    expect(actual).toEqual(expected);
+})
+
+test("getURLsFromHTML absolute", () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href="https://raiven.com/path/">
+        Raiven Site
+    </a>
+    </body>
+    </html>
+    `
+    const inputBaseURL = "https://raiven.com"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://raiven.com/path/"]
+    expect(actual).toEqual(expected);
+})
+
+test("getURLsFromHTML relative", () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href="/path/">
+        Raiven Site
+    </a>
+    </body>
+    </html>
+    `
+    const inputBaseURL = "https://raiven.com"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://raiven.com/path/"]
+    expect(actual).toEqual(expected);
+})
+
+test("getURLsFromHTML both", () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href="https://raiven.com/path1/">
+        Raiven Site 1
+    </a>
+    <a href="/path2/">
+        Raiven Site 2
+    </a>
+    </body>
+    </html>
+    `
+    const inputBaseURL = "https://raiven.com"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://raiven.com/path1/", "https://raiven.com/path2/"]
+    expect(actual).toEqual(expected);
+})
+
+
+test("getURLsFromHTML invalid", () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+    <a href="invalid">
+        Invalid URL
+    </a>
+    </body>
+    </html>
+    `
+    const inputBaseURL = "https://raiven.com"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected);
 })
